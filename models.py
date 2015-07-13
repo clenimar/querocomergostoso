ad94f6d347f872f8a9e9ab246a784e5e2c58bf5f
Clenimar Filemon <clenimar.filemon@gmail.com>
2015-07-13 03:16:16 -0300

    Regarding model relantionship w/ ndb.KeyProperty()

--- models.py
+++ models.py
@@ -1,14 +1,29 @@
 from google.appengine.ext import ndb
 
+
 class Owner(ndb.Model):
     name = ndb.StringProperty()
-    CPF = ndb.StringProperty()
+    cpf = ndb.StringProperty()
     birthday = ndb.DateProperty()
+
 
 class ItemMenu(ndb.Model):
     name = ndb.StringProperty()
     description = ndb.TextProperty()
     price = ndb.FloatProperty()
+
+
+class Order(ndb.Model):
+    items = ndb.StructuredProperty(ItemMenu, repeated=True)
+
+
+class Customer(ndb.Model):
+    email = ndb.StringProperty()
+    password = ndb.StringProperty()
+    name = ndb.StringProperty()
+    phone = ndb.StringProperty()
+    orders = ndb.KeyProperty(kind=Order, repeated=True)
+
 
 class Address(ndb.Model):
     street = ndb.StringProperty()
@@ -17,10 +32,6 @@ class Address(ndb.Model):
     postal_code = ndb.StringProperty()
     complement = ndb.StringProperty()
 
-class Order(ndb.Model):
-    client = ndb.StructuredProperty(Client)
-    restaurant = ndb.StructuredProperty(Restaurant)
-    items = ndb.StructuredProperty(ItemMenu, repeated=True)
 
 class Restaurant(ndb.Model):
     email = ndb.StringProperty()
@@ -28,12 +39,14 @@ class Restaurant(ndb.Model):
     name = ndb.StringProperty()
     owner = ndb.StructuredProperty(Owner)
     menu = ndb.StructuredProperty(ItemMenu, repeated=True)
-    orders = ndb.StructuredProperty(Order, repeated=True)
+    orders = ndb.KeyProperty(kind=Order, repeated=True)
 
-class Client(ndb.Model):
-    email = ndb.StringProperty()
-    password = ndb.StringProperty()
-    name = ndb.StringProperty()
-    phone = ndb.StringProperty()
-    orders = ndb.StructuredProperty(Order, repeated=True)
+    @staticmethod
+    def get_restaurants(self, restaurant_id=None):
+        if restaurant_id:
+            return Restaurant.query(Restaurant.id == restaurant_id)
+        return Restaurant.query()
 
+    @staticmethod
+    def create_restaurant(new):
+        new.put()
