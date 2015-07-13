56774aa144d49667cdd3b3ca1d7d93d7d675617c
Clenimar Filemon <clenimar.filemon@gmail.com>
2015-07-13 03:17:29 -0300

    API: getting and creating Restaurants

--- main.py
+++ main.py
@@ -13,16 +13,47 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
-#
+
+import models
 import webapp2
+
 
 class Project(webapp2.RequestHandler):
     def get(self):
-        self.response.write('olar.')
+        self.response.write('olars.')
+
+
+class Restaurant(webapp2.RequestHandler):
+    def get(self):
+        try:
+            restaurants = models.Restaurant.get_restaurants()
+            self.response.headers["Content-Type"] = "application/json"
+            self.response.out.write(restaurants)
+        except Exception:
+            self.error(500)
+
+    def post(self):
+        # creates a new Restaurant
+        new = models.Restaurant()
+        # changes its attributes
+        new.email = self.request.get('email')
+        new.pwd = self.request.get('password')
+        new.name = self.request.get('name')
+        new.owner = self.request.get('owner')
+
+        if self.request.get('menu'):
+            new.menu = self.request.get('menu')
+
+        # persists the new Restaurant
+        models.Restaurant.create_restaurant(new)
+
+        # TODO: return a JSON response
+
 
 #o index esta num arquivo separado chamado index.html que eh chamado no app.yaml
 app = webapp2.WSGIApplication([
-    ('/project', Project)
+    ('/dalton', Project),
+    ('/api/restaurant', Restaurant)
 ], debug=True)
 
 
